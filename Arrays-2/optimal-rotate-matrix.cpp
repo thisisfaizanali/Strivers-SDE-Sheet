4055ac5b132a6f02e85f6da8ac10e09d58647025
thisisfaizanali <faizanaliwhb@gmail.com>
2024-07-01 19:00:36 +0530

    optimal approach using transpose and then rotating

---
+++ Arrays-2/optimal-rotate-matrix.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        // first row becomes first column
+        // basically find transpose and reverse elements of the rows
+        int n = matrix.size();
+        for (int i = 0; i < n - 1; ++i)
+        {
+            for (int j = i + 1; j < n; ++j)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
