1b9a7f8fd29d9dcde869ed4adb708864f501784e
thisisfaizanali <faizanaliwhb@gmail.com>
2024-07-23 19:29:56 +0530

    brute, will give tle

---
+++ Arrays-3/2-a-brute-pow-bin-expo.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        // brute force approach
+        // will give tle
+        long long nn = n;
+        if (nn < 0)
+            nn = -1 * n;
+        double ans = 1;
+        for (long long i = 0; i < nn; ++i)
+        {
+            ans *= x;
+        }
+        if (n < 0)
+            ans = 1 / ans;
+        return ans;
+    }
+};
