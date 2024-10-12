38b9eb4607b9397a6b9ffec0a2f7a1f1261372e3
thisisfaizanali <faizanaliwhb@gmail.com>
2024-10-12 16:44:31 +0530

    Primary template for leetcode

---
+++ leetcodetemplate.cpp
@@ -0,0 +1,13 @@
+/*+++++++++++++++++++++++++++++++   Bismillah   ++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define eb emplace_back
+#define all(p) p.begin(), p.end()
+static auto _ = []()
+{ ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
+class Solution
+{
+public:
+};
