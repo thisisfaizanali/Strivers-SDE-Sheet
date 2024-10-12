d9f885ee9459b33a973cf53f5a9abf24bb750eb3
thisisfaizanali <faizanaliwhb@gmail.com>
2024-10-12 16:46:53 +0530

    Primary template for leetcode

--- leetcodetemplate.cpp
+++ leetcodetemplate.cpp
@@ -5,6 +5,7 @@ using ll = long long;
 #define pb push_back
 #define eb emplace_back
 #define all(p) p.begin(), p.end()
+#define idx size_t
 static auto _ = []()
 { ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
 class Solution
