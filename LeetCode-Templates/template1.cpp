6df3f384eb247adf4f2f5ffe855dd2a323c44fb8
thisisfaizanali <faizanaliwhb@gmail.com>
2024-11-11 16:01:44 +0530

    template 1 for contests

---
+++ LeetCode-Templates/template1.cpp
@@ -0,0 +1,10 @@
+/*+++++++++++++++++++++++++++++++   Bismillah   ++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define pb push_back
+#define eb emplace_back
+#define all(p) p.begin(), p.end()
+#define idx size_t
+static auto _ = []()
+{ ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
