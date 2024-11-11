74b61181ecf445bcc45b88ca8a32f4341dff508c
thisisfaizanali <faizanaliwhb@gmail.com>
2024-11-11 16:24:36 +0530

    for normal problem solving

---
+++ LeetCode-Templates/template3.cpp
@@ -0,0 +1,5 @@
+/*+++++++++++++++++++++++++++++++   Bismillah   ++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+static auto _ = []()
+{ ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
