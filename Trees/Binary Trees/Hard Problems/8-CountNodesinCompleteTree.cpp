0d9905863a01b5e31f1e9ee82afe345d880d0861
thisisfaizanali <faizanaliwhb@gmail.com>
2025-03-17 19:41:29 +0530

    count nodes in CBT

---
+++ Trees/Binary Trees/Hard Problems/8-CountNodesinCompleteTree.cpp
@@ -0,0 +1,48 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++  Bismillah  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+class Solution
+{
+public:
+    // For height do not use recursion :
+    int leftHeight(TreeNode *root)
+    {
+        int height = 0;
+        while (root)
+        {
+            height++;
+            root = root->left;
+        }
+        return height;
+    }
+    int rightHeight(TreeNode *root)
+    {
+        int height = 0;
+        while (root)
+        {
+            height++;
+            root = root->right;
+        }
+        return height;
+    }
+    int countNodes(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        int leftSubtreeHeight = leftHeight(root);
+        int rightSubtreeHeight = rightHeight(root);
+        if (leftSubtreeHeight == rightSubtreeHeight)
+            return (1 << leftSubtreeHeight) - 1;
+        else
+            return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
