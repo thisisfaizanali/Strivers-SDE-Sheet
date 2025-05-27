bc349ff909c56edf1c88a00984d81d0591bd4e0a
thisisfaizanali <faizanaliwhb@gmail.com>
2025-05-28 01:19:04 +0530

    added the missing findNode function

--- Trees/Binary Trees/Hard Problems/7-BurningTree.cpp
+++ Trees/Binary Trees/Hard Problems/7-BurningTree.cpp
@@ -15,33 +15,6 @@ struct Node
 class Solution
 {
 public:
-    void markParent(Node *root, unordered_map<Node *, Node *> &parent, Node *&targetNode, int target)
-    {
-        // mark Parent and find target Node :
-        queue<Node *> q;
-        q.push(root);
-        while (!q.empty())
-        {
-            int size = q.size();
-            for (int i = 0; i < size; ++i)
-            {
-                Node *node = q.front();
-                q.pop();
-                if (node->data == target)
-                    targetNode = node;
-                if (node->left)
-                {
-                    q.push(node->left);
-                    parent[node->left] = node;
-                }
-                if (node->right)
-                {
-                    q.push(node->right);
-                    parent[node->right] = node;
-                }
-            }
-        }
-    }
     int minTime(Node *root, int target)
     {
         // bt to ug and find target node :
@@ -81,5 +54,48 @@ public:
         }
         return time - 1; // while loop condition is checked at start, that's why -1
         // alternatively use a flag
+    }
+
+private:
+    Node *findNode(Node *root, int target)
+    {
+        if (!root)
+            return nullptr;
+        if (root->data == target)
+            return root;
+        Node *leftSearch = findNode(root->left, target);
+        if (leftSearch)
+            return leftSearch;
+        else
+            return findNode(root->right, target);
+    }
+
+private:
+    void markParent(Node *root, unordered_map<Node *, Node *> &parent, Node *&targetNode, int target)
+    {
+        // mark Parent and find target Node :
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; ++i)
+            {
+                Node *node = q.front();
+                q.pop();
+                if (node->data == target)
+                    targetNode = node;
+                if (node->left)
+                {
+                    q.push(node->left);
+                    parent[node->left] = node;
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
+                    parent[node->right] = node;
+                }
+            }
+        }
     }
 };
