21a6ed181377fdff2c27af8217dff24f83dafca1
thisisfaizanali <faizanaliwhb@gmail.com>
2025-06-24 17:16:18 +0530

    refactor: remove unused template3.cpp file

--- LeetCode-Templates/template3.cpp
+++
@@ -1,5 +0,0 @@
-/*+++++++++++++++++++++++++++++++   Bismillah   ++++++++++++++++++++++++++++++*/
-#include <bits/stdc++.h>
-using namespace std;
-static auto _ = []()
-{ ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
