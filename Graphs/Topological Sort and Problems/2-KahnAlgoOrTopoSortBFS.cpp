265f794da94b7da6e098bc4b0f21804f64682b16
thisisfaizanali <faizanaliwhb@gmail.com>
2025-07-02 02:49:36 +0530

    feat: implement Kahn's algorithm for topological sorting using BFS

---
+++ Graphs/Topological Sort and Problems/2-KahnAlgoOrTopoSortBFS.cpp
@@ -0,0 +1,64 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++  Bismillah  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> topoSort(int V, vector<vector<int>> &edges)
+    {
+        // using bfs or kahn's algorithm :
+
+        // we have to create adjacency list for DAG :
+        vector<vector<int>> adj(V);
+        for (auto &edge : edges)
+            adj[edge[0]].push_back(edge[1]);
+
+        // calculate the indegree :
+        vector<int> indegree(V, 0);
+
+        for (int u = 0; u < V; ++u)
+        {
+            for (auto &v : adj[u])
+            {
+                indegree[v]++;
+            }
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < V; ++i)
+        {
+            if (indegree[i] == 0)
+            {
+                // push nodes with indegree 0 to queue:
+                q.push(i);
+            }
+        }
+
+        vector<int> ans;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node); // because the node already has indegree 0
+            // and we push to queue only when the indegree is 0 :
+
+            for (auto &neighbour : adj[node])
+            {
+                indegree[neighbour]--; // remove the contribution of node on neighbour
+                // because it is already placed in ans
+
+                if (indegree[neighbour] == 0)
+                {
+                    // push to queue when indegree is 0 :
+                    q.push(neighbour);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
