5fccbed1090aff89e55fdd38a7da6cffc8f9897b
thisisfaizanali <faizanaliwhb@gmail.com>
2025-07-11 19:00:08 +0530

    feat: implement Prim's algorithm for Minimum Spanning Tree in 1-primAlgo.cpp feat: add initial structure for Kruskal's algorithm in 2-KruskalAlgo.cpp

---
+++ Graphs/MST and DSU problems/2-KruskalAlgo.cpp
@@ -0,0 +1,11 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++  Bismillah  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+    }
+};
