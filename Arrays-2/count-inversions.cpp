e09837ba785fb32d9d6c1b3ae1995142c5bdba89
thisisfaizanali <faizanaliwhb@gmail.com>
2025-08-14 00:25:02 +0530

    fix : remove unnecessary code for faster i/o

--- Arrays-2/count-inversions.cpp
+++ Arrays-2/count-inversions.cpp
@@ -1,8 +1,6 @@
 /*+++++++++++++++++++++++++++++++   Bismillah   ++++++++++++++++++++++++++++++*/
 #include <bits/stdc++.h>
 using namespace std;
-static auto _ = []()
-{ ios::sync_with_stdio(false); cin.tie(nullptr); return nullptr; }();
 class Solution
 {
 public:
